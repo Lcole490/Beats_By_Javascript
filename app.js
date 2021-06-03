729c296619eac81051b688b395f21dc2e4af052f
Lcole.engr <lcole.engr@gmail.com>
2021-06-03 09:49:42 -0400

    incorporated click event to drumpads to toggle between active class and default class

--- app.js
+++ app.js
@@ -3,24 +3,37 @@
 class DrumKit{
     constructor(){
         this.pads = document.querySelectorAll('.pad');
+        this.playBtn = document.querySelector('.play')
         this.kickAudio = document.querySelector('.kick-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
 
         this.index= 0; // Monitors the status of the track, or progression point of the track
+
+        this.bpm = 150;
     }
 
-    repeat(){
+    activePad(){
+        this.classList.toggle('active');
+    }
+
+    repeat(){               // Method to repeat steps of the track 
         let step = this.index % 8;
-        console.log(step);
+        console.log(`Step ${step}, Index ${this.index}`);
         this.index++;
+        const activeBars = document.querySelectorAll(`.b${step}`);
+        console.log(activeBars);
+
+
     }
 
-    start(){
+    start(){            // Method to set the interval at which the step would increment. in this case a step is added every second
+
+        const interval = (60/this.bpm) * 1000;
         setInterval(()=> {
 
             this.repeat();
-        }, 1000);
+        }, interval);
     }
 }
 
@@ -28,4 +41,12 @@ class DrumKit{
 
 const drumKit = new DrumKit();
 
-drumKit.start();
+
+drumKit.pads.forEach(pad => {
+    pad.addEventListener('click', drumKit.activePad)
+})
+
+
+drumKit.playBtn.addEventListener("click", ()=> {
+    drumKit.start();
+})
